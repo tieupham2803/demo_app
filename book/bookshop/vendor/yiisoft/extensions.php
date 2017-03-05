2635683d705af01644b12ae3c57814608d97123e
Tieupham <tieupham2803@gmail.com>
2017-03-05 19:52:13 +0700

    add332017

---
+++ book/bookshop/vendor/yiisoft/extensions.php
@@ -0,0 +1,87 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '2.0.5.0',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer',
+    ),
+  ),
+  'mihaildev/ckeditor' => 
+  array (
+    'name' => 'mihaildev/ckeditor',
+    'version' => '2',
+    'alias' => 
+    array (
+      '@mihaildev/ckeditor' => $vendorDir . '/mihaildev/ckeditor',
+    ),
+  ),
+  'yiisoft/yii2-codeception' => 
+  array (
+    'name' => 'yiisoft/yii2-codeception',
+    'version' => '2.0.5.0',
+    'alias' => 
+    array (
+      '@yii/codeception' => $vendorDir . '/yiisoft/yii2-codeception',
+    ),
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '2.0.5.0',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii',
+    ),
+  ),
+  'yiisoft/yii2-faker' => 
+  array (
+    'name' => 'yiisoft/yii2-faker',
+    'version' => '2.0.3.0',
+    'alias' => 
+    array (
+      '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker',
+    ),
+  ),
+  'yiisoft/yii2-jui' => 
+  array (
+    'name' => 'yiisoft/yii2-jui',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/jui' => $vendorDir . '/yiisoft/yii2-jui',
+    ),
+  ),
+  'mihaildev/yii2-elfinder' => 
+  array (
+    'name' => 'mihaildev/yii2-elfinder',
+    'version' => '1.2.1.0',
+    'alias' => 
+    array (
+      '@mihaildev/elfinder' => $vendorDir . '/mihaildev/yii2-elfinder',
+    ),
+  ),
+);
---
+++ book/frontend/controllers/CheckoutController.php
@@ -0,0 +1,47 @@
+<?php 
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use frontend\common\Cart;
+use frontend\models\Checkout;
+
+
+/**
+* 
+*/
+class CheckoutController extends Controller
+{
+	
+	function actionIndex()
+	{
+		
+		$model = new Checkout();
+
+		$cart = new Cart();
+		$cartstore = $cart->cartstore;
+		$cost = $cart->getCost;
+
+		if ($model->load(Yii::$app->request->post())) {
+
+			if ($cus = $model->customer()) {var_dump($cus); die;
+				
+				if ($order = $model->order($cus->id)) {
+			// 	
+				$model->order_item($order->id);
+			}
+			}
+		}
+	
+	return $this->render('index',[
+
+			'model' => $model,
+			'cartstore' => $cartstore,
+			'cost' => $cost
+
+		]
+
+		);
+}}
+
+ ?>
